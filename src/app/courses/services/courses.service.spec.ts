3871b8e70dd5a78bcd92dbae42190a7d7c71f3b2
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-04-22 18:20:43 -0400

    S2.16-checking http parameters

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -4,7 +4,7 @@ import {
   HttpTestingController,
 } from "@angular/common/http/testing";
 import { TestBed } from "@angular/core/testing";
-import { COURSES } from "../../../../server/db-data";
+import { COURSES, findLessonsForCourse, LESSONS } from "../../../../server/db-data";
 import { Course } from "../model/course";
 import { CoursesService } from "./courses.service";
 
@@ -80,20 +80,44 @@ describe("CoursesService", () => {
       titles: { description: "Testing Course" },
     };
 
-    coursesService.saveCourse(12, changes)
-      .subscribe(
-        () => fail("this save course operation should have failed"),
-        (error: HttpErrorResponse) => {
-          expect(error.status).toBe(500);
-        }
-      );
+    coursesService.saveCourse(12, changes).subscribe(
+      () => fail("this save course operation should have failed"),
+      (error: HttpErrorResponse) => {
+        expect(error.status).toBe(500);
+      }
+    );
 
     const req = httpTestingController.expectOne("/api/courses/12");
 
     expect(req.request.method).toEqual("PUT");
 
-    req.flush("Save course failed", {status:500,
-      statusText:"Internal Server Error"});
+    req.flush("Save course failed", {
+      status: 500,
+      statusText: "Internal Server Error",
+    });
+  });
+
+  it("should find a list of lessons", () => {
+    coursesService.findLessons(12).subscribe((lessons) => {
+      expect(lessons).toBeTruthy();
+
+      expect(lessons.length).toBe(3);
+    });
+
+    const req = httpTestingController.expectOne(
+      (req) => req.url == "/api/lessons"
+    );
+
+    expect(req.request.method).toEqual("GET");
+    expect(req.request.params.get("courseId")).toEqual("12");
+    expect(req.request.params.get("filter")).toEqual("");
+    expect(req.request.params.get("sortOrder")).toEqual("asc");
+    expect(req.request.params.get("pageNumber")).toEqual("0");
+    expect(req.request.params.get("pageSize")).toEqual("3");
+
+    req.flush({
+      payload: findLessonsForCourse(12).slice(0,3)
+    });
   });
 
   afterEach(() => {
