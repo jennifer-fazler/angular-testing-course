789c949d0f07b2f73621f92fad5f743187e3ebe8
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-04-22 15:55:23 -0400

    S1.5-quick intro

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,12 @@
+describe('CalculatorService', () => {
+
+   // meant to test a functional feature, not a technical spec
+  it('should add two numbers', () => {
+    pending();
+  });
+
+  it('should subtract two numbers', () => {
+    fail();
+  });
+
+});
