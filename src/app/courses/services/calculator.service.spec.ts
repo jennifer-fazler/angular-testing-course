a5094f57cf9d9a0f5bc53c51caed813e7bcd8298
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-04-22 16:20:28 -0400

    S1.7-intro to jasmine spy

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -5,11 +5,21 @@ describe('CalculatorService', () => {
 
    // meant to test a functional feature, not a technical spec
   it('should add two numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
+    // const logger = new LoggerService(); // ususally we want to provide mock or fake services for the unit test
+
+    // this is a complete fake object
+    const logger = jasmine.createSpyObj('LoggerService', ["log"]);
+
+    const calculator = new CalculatorService(logger);
+
+    // now not needed because jasmine will automatically spy on the spyobj
+    // spyOn(logger, 'log');
 
     const result = calculator.add(2,2);
 
     expect(result).toBe(4);
+
+    expect(logger.log).toHaveBeenCalledTimes(1);
   });
 
   it('should subtract two numbers', () => {
