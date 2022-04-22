13f244c2f2f5667f3a7178022b6c737c5d69665b
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-04-22 16:09:53 -0400

    S1.6-first jasmine specs

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,12 +1,23 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
 describe('CalculatorService', () => {
 
    // meant to test a functional feature, not a technical spec
   it('should add two numbers', () => {
-    pending();
+    const calculator = new CalculatorService(new LoggerService());
+
+    const result = calculator.add(2,2);
+
+    expect(result).toBe(4);
   });
 
   it('should subtract two numbers', () => {
-    fail();
+    const calculator = new CalculatorService(new LoggerService());
+
+    const result = calculator.subtract(2,2);
+
+    expect(result).toBe(0, "unexpected subtraction result");
   });
 
 });
