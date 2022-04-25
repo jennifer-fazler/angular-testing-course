e1a6c602863b742745bb2c7ab7f64de77f072ea1
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-04-25 17:27:36 -0400

    S4.39 simulate user interaction in end to end tests

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -1,6 +1,6 @@
 describe('Home Page', () => {
-  it('should display a list of courses', () => {
 
+  beforeEach(() => {
     // could not get this to work for some reason
     // cy.fixture('courses.json').as('coursesJSON');
     // cy.intercept('/api/courses', '@coursesJSON').as("courses");
@@ -9,10 +9,23 @@ describe('Home Page', () => {
     cy.intercept('GET', '/api/courses', { fixture: 'courses.json'}).as("courses");
 
     cy.visit('/');
+  });
+
+  it('should display a list of courses', () => {
     cy.contains("All Courses");
 
     cy.wait("@courses");
 
     cy.get("mat-card").should("have.length", 9);
+  });
+
+  it('should display the advanced courses', () => {
+    cy.get('.mat-tab-label').should('have.length', 2);
+
+    cy.get('.mat-tab-label').last().click();
+
+    cy.get('.mat-tab-body-active .mat-card-title').its('length').should('be.gt', 1);
+
+    cy.get('.mat-tab-body-active .mat-card-title').first().should('contain', 'Angular Security Course');
   });
 });
