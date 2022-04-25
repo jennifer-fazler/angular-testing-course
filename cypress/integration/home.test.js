bd67cc5a32499cc9d00efbd7145f7bfaf84603c1
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-04-25 17:21:03 -0400

    S5.38 how to mock HTTP backend using cypress

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -1,6 +1,18 @@
 describe('Home Page', () => {
   it('should display a list of courses', () => {
+
+    // could not get this to work for some reason
+    // cy.fixture('courses.json').as('coursesJSON');
+    // cy.intercept('/api/courses', '@coursesJSON').as("courses");
+
+    // but this worked (from https://docs.cypress.io/api/commands/fixture#Using-the-fixture-StaticResponse-property)
+    cy.intercept('GET', '/api/courses', { fixture: 'courses.json'}).as("courses");
+
     cy.visit('/');
     cy.contains("All Courses");
+
+    cy.wait("@courses");
+
+    cy.get("mat-card").should("have.length", 9);
   });
 });
