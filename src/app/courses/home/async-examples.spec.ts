bfffdd74d7ae01feb724718c9a00f66f7a8b56e0
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-04-25 14:54:41 -0400

    S4.28 understanding fakeAsync zone

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,3 +1,5 @@
+import { fakeAsync, flush, tick } from "@angular/core/testing";
+
 fdescribe("Async Testing Examples", () => {
   // This approach should be avoided.  Hard to know how long to wait, and might exceed timeout. nested timeout blocks, etc
   it("Asynchronous test example with Jasmine done()", (done: DoneFn) => {
@@ -11,5 +13,34 @@ fdescribe("Async Testing Examples", () => {
 
       done();
     }, 1000);
+    expect(test).toBeFalsy();
   });
+
+  it("Asynchronous test example - setTimeout()", fakeAsync(() => {
+    let test = false;
+
+    setTimeout(() => {}); // adding a 2nd one changes error to 2 timer(s) still in the queue
+
+    setTimeout(() => {
+      console.log("running assertions setTimeout()");
+      test = true;
+
+      // can remove this test. we don't have to write the assertions in the nested code blocks when using fakeAsync and tick
+      // expect(test).toBeTruthy();
+    }, 1000);
+
+    expect(test).toBeFalsy();
+
+    // method 1: use tick to move time forward
+    // tick(500); // move time forward 500ms
+    // // still fails if we stop here
+    // tick(499); // move time forward 499ms
+    // // still fails if we stop here
+    // tick(1); // move time forward 1ms
+
+    // method 2: use flush to make sure all of the async operations timeouts have been executed before moving on.
+    flush();
+
+    expect(test).toBeTruthy();
+  }));
 });
